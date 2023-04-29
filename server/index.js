1f992566f22dbcd36dbb97ad536b08291d91ead2
Saleem-devs <seleemadeniyi10@gmail.com>
2023-04-29 11:13:42 +0100

    server

--- server/index.js
+++ server/index.js
@@ -2,8 +2,6 @@ import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
 
-import connectDB from "./mongodb/connect.js";
-import postRoutes from "./routes/postRoutes.js";
 import dalleRoutes from "./routes/dalleRoutes.js";
 
 dotenv.config();
@@ -12,17 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
-app.use("/api/v1/post", postRoutes);
 app.use("/api/v1/dalle", dalleRoutes);
-
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
 
 app.get("/", async (req, res) => {
   res.send("Hello from PixelAI!");
@@ -30,7 +18,6 @@ app.get("/", async (req, res) => {
 
 const startServer = async () => {
   try {
-    connectDB(process.env.MONGODB_URL);
     app.listen(8080, () =>
       console.log("server is running on port http://localhost:8080")
     );
